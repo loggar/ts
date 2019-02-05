c60d187750131d6c73ea0df4a10831478edf6538
charly.lee <webnl@DT-Charly.koi.local>
2019-02-05 15:43:19 +1100

    feat (tsc compile) vscode task

--- ts-core/declarations/declarations.js
+++ ts-core/declarations/declarations.js
@@ -1,11 +1,10 @@
-"use strict";
 var n = 1;
 var s = "str";
 var Ns;
 (function (Ns) {
-    Ns.x = 'x in Ns';
+    Ns.x = "x in Ns";
 })(Ns || (Ns = {}));
-console.log('Ns=' + JSON.stringify(Ns));
+console.log("Ns=" + JSON.stringify(Ns));
 var Cl = /** @class */ (function () {
     function Cl() {
         this.x = "str"; // instance member
@@ -13,6 +12,6 @@ var Cl = /** @class */ (function () {
     Cl.x = 1; // static menber
     return Cl;
 }());
-console.log('Cl.x=' + Cl.x);
+console.log("Cl.x=" + Cl.x);
 var cl1 = new Cl();
-console.log('cl1.x=' + cl1.x);
+console.log("cl1.x=" + cl1.x);
