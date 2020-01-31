735298c1e7f96ead05a62c7401a2b9912b7fba38
Charly LEE <charly.loggar@gmail.com>
2020-01-31 16:39:41 +1100

    feat (type) type guards

--- ts-core/type-combinations/intersection.2.ts
+++ ts-core/type-combinations/intersection.2.ts
@@ -1,9 +1,9 @@
 type Person2 = { name: string; age: number };
 type Worker2 = { companyId: string };
 
-type Employee = Person2 & Worker2;
+type Employee2 = Person2 & Worker2;
 
-const bestOfTheMonth: Employee = {
+const bestOfTheMonth: Employee2 = {
   name: "Peter",
   age: 39,
   companyId: "123456"
